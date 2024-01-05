1932892a651664c9199dd6625a2291b9af4bd77e
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2024-01-05 17:28:07 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return Article();
+
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">An Article</h2>
+      <label htmlFor="myFirstInput">Pls give me some input:</label>
+      <input id="myFirstInput" type="text" />
+      <a
+        className="article__link"
+        href="https://react.dev"
+      >
+        Click me!
+      </a>
+    </article>
+  );
 }
